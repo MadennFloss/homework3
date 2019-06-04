5844954cf457914c4bb143a7fa5162d7b6c57ee1
MadennFloss <47519011+MadennFloss@users.noreply.github.com>
2019-06-04 16:26:23 +0300

    Add files via upload

---
+++ formatter_ex_lib/_build/CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/home/madennfloss/MadennFloss/workspace/tasks/lab03/solver_lib/solver.cpp.o"
+  "libsolver.pdb"
+  "libsolver.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
