722fbbcfd3ac0e86a33f048365296abc58155469
MadennFloss <47519011+MadennFloss@users.noreply.github.com>
2019-06-04 16:31:29 +0300

    Add files via upload

---
+++ hello_world_application/_build/CMakeFiles/formatter_ex.h.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatter_ex.h.dir/home/madennfloss/MadennFloss/workspace/tasks/lab03/formatter_ex_lib/formatter_ex.cpp.o"
+  "libformatter_ex.h.pdb"
+  "libformatter_ex.h.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatter_ex.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
